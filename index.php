25f90c98845cfb928b529329d7fd42918caf55e3
Yaiser <yaiserar@gmail.com>
2021-11-10 17:15:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- TODO Application entry point. Login view -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/style.css" rel="stylesheet">
+  <title>Employee Management</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
